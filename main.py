c9aeb11a8ac689bf95bcdcb2293df7f7b1cbe4af
Balaguru Sivasambagupta <bala1802@live.com>
2023-08-03 06:43:01 +0530

    modified the number of epochs

--- main.py
+++ main.py
@@ -129,7 +129,7 @@ def test(epoch):
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
 
-for epoch in range(start_epoch, start_epoch+200):
+for epoch in range(start_epoch, start_epoch+20):
     train(epoch)
     test(epoch)
     scheduler.step()
